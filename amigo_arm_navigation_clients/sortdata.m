b04308715923b8a4039ffeccf3f85cc5e17f2f5e
Nic Dirkx <n.j.dirkx@student.tue.nl>
2011-02-02 11:51:11 +0000

    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/trunk/amigo_arm_navigation@1559 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

---
+++ amigo_arm_navigation_clients/sortdata.m
@@ -0,0 +1,34 @@
+close all
+
+
+x = data(1,:);
+
+%in case of pos + vel, row_length = 15
+%in case of pos + vel+acc, row_length = 22
+
+row_length = 22;
+
+nr_points = length(x)/row_length;
+pos = zeros(nr_points,7);
+vel = zeros(nr_points,7);
+acc = zeros(nr_points,7);
+time = zeros(nr_points,1);
+
+for i=1:nr_points
+   elem = row_length *(i-1) +1; 
+   pos(i,1:7) =  x(1, elem:(elem+6));
+   time(i) = x(1, row_length * i); 
+    
+end
+
+plot(time, pos(:,1),'b')
+hold
+plot(time, pos(:,2),'r')
+plot(time, pos(:,3),'k')
+plot(time, pos(:,4),'g')
+plot(time, pos(:,5),'m')
+plot(time, pos(:,6),'c')
+plot(time, pos(:,7),'y')
+title('Filtered trajectory');
+xlabel('time [s]')
+ylabel('joint angles [rad]')
