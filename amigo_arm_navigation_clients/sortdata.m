e64045632732a0ba16412264d443772d7f48a766
Nic Dirkx <n.j.dirkx@student.tue.nl>
2011-02-07 16:32:23 +0000

    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/trunk/amigo_arm_navigation@1604 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- amigo_arm_navigation_clients/sortdata.m
+++ amigo_arm_navigation_clients/sortdata.m
@@ -7,7 +7,7 @@ x = unfiltered(1,:);
 %in case of pos , row_length = 8
 %in case of pos + vel, row_length = 15
 %in case of pos + vel+acc, row_length = 22
-num_joints = 2;
+num_joints = 7;
 row_length = num_joints + 1;
 
 nr_points = length(x)/row_length
@@ -19,28 +19,35 @@ time = zeros(nr_points,1);
 
 for i=1:nr_points
    elem = row_length *(i-1) +1; 
-   pos(i,1:num_joints) =  x(1, elem:(elem+(num_joints-1)))
-   time(i) = x(1, row_length * i) 
+   pos(i,1:num_joints) =  x(1, elem:(elem+(num_joints-1)));
+   time(i) = x(1, row_length * i) ;
     
 end
+
+
+diff_q = diff(pos);
+
 figure
 subplot(2,2,1)
 plot([0 :nr_points-1], pos(:,1),'b')
 hold
 plot([0 :nr_points-1], pos(:,2),'r')
-%plot(time, pos(:,3),'k')
-%plot(time, pos(:,4),'g')
-%plot(time, pos(:,5),'m')
-%plot(time, pos(:,6),'c')
-plot(time, pos(:,num_joints),'y')
+plot([0 :nr_points-1], pos(:,3),'k')
+plot([0 :nr_points-1], pos(:,4),'g')
+plot([0 :nr_points-1], pos(:,5),'m')
+plot([0 :nr_points-1], pos(:,6),'c')
+plot([0 :nr_points-1], pos(:,num_joints),'y')
 title('Unfiltered trajectory');
 xlabel('seq')
 ylabel('joint angles [rad]')
 grid
-subplot(2,2,3)
+subplot(2,2,4)
 plot(pos(:,1),pos(:,2))
 grid
 
+subplot(2,2,3)
+plot([0 :nr_points-2],diff_q)
+grid
 %% FILTERED
 
 x = filtered(1,:);
@@ -82,7 +89,7 @@ ylabel('joint angles [rad]')
 grid
 
 
-subplot(2,2,3)
+subplot(2,2,4)
 hold on
 plot(pos(:,1),pos(:,2),'g')
 grid on
