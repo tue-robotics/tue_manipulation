af190c2c8e72b60fcb5f07d47a24e4c028d64597
Tim Clephas <t.t.g.clephas@alumnus.tue.nl>
2016-12-10 14:11:41 +0100

    2to3

--- test/test_moveit.py
+++ test/test_moveit.py
@@ -5,7 +5,7 @@ import moveit_commander
 import moveit_msgs.msg
 import geometry_msgs.msg
 
-print "============ Start testing. First run: export ROS_NAMESPACE=amigo"
+print("============ Start testing. First run: export ROS_NAMESPACE=amigo")
 
 moveit_commander.roscpp_initialize(sys.argv)
 rospy.init_node('move_group_python_interface_tutorial',
@@ -22,18 +22,18 @@ display_trajectory_publisher = rospy.Publisher(
                                     moveit_msgs.msg.DisplayTrajectory,
                                     queue_size=10)
 
-print "============ Reference frame: %s" % group.get_planning_frame()
+print("============ Reference frame: %s" % group.get_planning_frame())
 
 group.set_pose_reference_frame('/amigo/base_link')
 
-print "============ Reference frame: %s" % group.get_end_effector_link()
+print("============ Reference frame: %s" % group.get_end_effector_link())
 
-print "============ Robot Groups:"
-print robot.get_group_names()
+print("============ Robot Groups:")
+print(robot.get_group_names())
 
-print "============ Printing robot state"
-print robot.get_current_state()
-print "============"
+print("============ Printing robot state")
+print(robot.get_current_state())
+print("============")
 
 
 # print "============ Generating plan 1"
@@ -43,8 +43,8 @@ group.set_pose_target(pose_target)
 
 plan1 = group.plan()
 
-print "============ Waiting while RVIZ displays plan1..."
+print("============ Waiting while RVIZ displays plan1...")
 rospy.sleep(5)
 
-print "============ Execute plan"
+print("============ Execute plan")
 group.go(wait=True)
